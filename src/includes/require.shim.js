f01ccc0ab067aa1e8767749add9134280d0d690d
Cristian Bote <botecristian@yahoo.com>
2015-03-17 23:31:44 +0200

    Added AMD and requirejs module support and no version dist file

---
+++ src/includes/require.shim.js
@@ -0,0 +1,21 @@
+/**
+ * Defines the register method for a generic, AMD and require modules definition
+ */
+var register = function(moduleName, definition){
+    "use strict";
+
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            window.exports = module.exports = definition;
+        }
+        exports[moduleName] = definition;
+    } else {
+        if (typeof define !== 'undefined') {
+            define(moduleName, function() {
+                return definition;
+            });
+        } else {
+            window[moduleName] = definition;
+        }
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,136 +1,140 @@
-(function(window, moduleName){
-    "use strict";
+register('Easyflux',
+    (function(window){
+        "use strict";
 
-    /**
-     * Creates buckets of events passed in as array
-     * Easier to namespace events
-     * @type {Function}
-     */
-    var module = window[moduleName] = function (bucket) {
-        var bundle;
+        /**
+         * Creates buckets of events passed in as array
+         * Easier to namespace events
+         * @type {Function}
+         */
+        var module = function (bucket) {
+            var bundle;
 
-        if (bucket && bucket.length) {
+            if (bucket && bucket.length) {
 
-            bundle = {};
+                bundle = {};
 
-            for (var i = 0; i < bucket.length; i++) {
-                bundle[bucket[i]] = {};
+                for (var i = 0; i < bucket.length; i++) {
+                    bundle[bucket[i]] = {};
 
-                (function(){
+                    (function(){
 
-                    bundle[bucket[i]] = {
+                        bundle[bucket[i]] = {
 
-                        /**
-                         * Event ID
-                         */
-                        NAME: bucket[i],
+                            /**
+                             * Event ID
+                             */
+                            NAME: bucket[i],
 
-                        /**
-                         * Event trigger method
-                         * @param [values] []
-                         */
-                        trigger: function() {
-                            var args = Array.prototype.slice.apply(arguments);
+                            /**
+                             * Event trigger method
+                             * @param [values] []
+                             */
+                            trigger: function() {
+                                var args = Array.prototype.slice.apply(arguments);
 
-                            // Clean way of grabbing params
-                            this.callbacks = this.callbacks || [];
+                                // Clean way of grabbing params
+                                this.callbacks = this.callbacks || [];
 
-                            for (var i = 0; i < this.callbacks.length; i++) {
-                                this.callbacks[i].callback.apply(this.callbacks[i].scope, args);
-                            }
-
-                        }.bind(bundle[bucket[i]]),
-
-                        /**
-                         * Listener method
-                         * @param callback {Function}
-                         * @param scope {React.component|*}
-                         */
-                        listen: function(callback, scope) {
-                            this.callbacks = this.callbacks || [];
-                            this.callbacks.push({
-                                callback: callback,
-                                scope: scope
-                            });
-                        }.bind(bundle[bucket[i]]),
-
-                        /**
-                         * Remove listener for this event at this scope
-                         * @param scope {*}
-                         */
-                        remove: function(scope) {
-                            for (var i = 0; i < this.callbacks.length; i++) {
-                                if (this.callbacks[i].scope === scope) {
-                                    break;
+                                for (var i = 0; i < this.callbacks.length; i++) {
+                                    this.callbacks[i].callback.apply(this.callbacks[i].scope, args);
                                 }
-                            }
 
-                            this.callbacks.splice(i, 1);
-                        }.bind(bundle[bucket[i]])
+                            }.bind(bundle[bucket[i]]),
 
-                    };
+                            /**
+                             * Listener method
+                             * @param callback {Function}
+                             * @param scope {React.component|*}
+                             */
+                            listen: function(callback, scope) {
+                                this.callbacks = this.callbacks || [];
+                                this.callbacks.push({
+                                    callback: callback,
+                                    scope: scope
+                                });
+                            }.bind(bundle[bucket[i]]),
 
-                }());
-            }
-        }
+                            /**
+                             * Remove listener for this event at this scope
+                             * @param scope {*}
+                             */
+                            remove: function(scope) {
+                                for (var i = 0; i < this.callbacks.length; i++) {
+                                    if (this.callbacks[i].scope === scope) {
+                                        break;
+                                    }
+                                }
 
-        return bundle;
-    };
+                                this.callbacks.splice(i, 1);
+                            }.bind(bundle[bucket[i]])
 
-    module.Mixin = {
-        /**
-         * Holds the references for events to unmount when components unmounts
-         * @private
-         */
-        _toUnmout: [],
+                        };
 
-        /**
-         * General listener method
-         * @param eventObj
-         * @param callback
-         * @param scope
-         */
-        listenTo: function(eventObj, callback, scope) {
-
-            // Grab the eventObj from context.events
-            if (typeof eventObj === 'string') {
-                eventObj = this.events[eventObj];
-            }
-
-            eventObj.listen(callback, scope);
-
-            this._toUnmout.push({
-                eventObj: eventObj,
-                scope: scope
-            });
-        },
-
-        /**
-         * Triggers a certain event
-         * @param eventObj {*}
-         * @param [values] []
-         */
-        trigger: function(eventObj) {
-            var args = Array.prototype.slice.apply(arguments);
-            args.shift();
-
-            // Grab the eventObj from context.events
-            if (typeof eventObj === 'string') {
-                eventObj = this.events[eventObj];
+                    }());
+                }
             }
 
-            eventObj.trigger.apply(eventObj, args);
-        },
+            return bundle;
+        };
 
-        /**
-         * Overrides the default handler for unmounting components
-         * @override
-         */
-        componentWillUnmount: function() {
-            for (var i = 0; i < this._toUnmout.length; i++) {
-                this._toUnmout[i].eventObj.remove(this);
+        module.Mixin = {
+            /**
+             * Holds the references for events to unmount when components unmounts
+             * @private
+             */
+            _toUnmout: [],
+
+            /**
+             * General listener method
+             * @param eventObj
+             * @param callback
+             * @param scope
+             */
+            listenTo: function(eventObj, callback, scope) {
+
+                // Grab the eventObj from context.events
+                if (typeof eventObj === 'string') {
+                    eventObj = this.events[eventObj];
+                }
+
+                eventObj.listen(callback, scope);
+
+                this._toUnmout.push({
+                    eventObj: eventObj,
+                    scope: scope
+                });
+            },
+
+            /**
+             * Triggers a certain event
+             * @param eventObj {*}
+             * @param [values] []
+             */
+            trigger: function(eventObj) {
+                var args = Array.prototype.slice.apply(arguments);
+                args.shift();
+
+                // Grab the eventObj from context.events
+                if (typeof eventObj === 'string') {
+                    eventObj = this.events[eventObj];
+                }
+
+                eventObj.trigger.apply(eventObj, args);
+            },
+
+            /**
+             * Overrides the default handler for unmounting components
+             * @override
+             */
+            componentWillUnmount: function() {
+                for (var i = 0; i < this._toUnmout.length; i++) {
+                    this._toUnmout[i].eventObj.remove(this);
+                }
             }
-        }
-    };
+        };
 
-}(window, 'Easyflux'));
+        return module;
+
+    }(window))
+);
